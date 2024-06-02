ba96d1366559bb852a48b0fc2622381b823ab68f
Arshiya Kishore <135497076+arshiyaakishore@users.noreply.github.com>
2024-06-02 13:06:48 +0530

    Update javascript.js

--- javascript.js
+++ javascript.js
@@ -22,3 +22,25 @@ if(mystery[0] === 'P' && mystery.length > 5 && mystery.indexOf('7') !== -1){
 -----------
 //Array definition
 let lottoNumbers=[1,2,3,4,5,6];
+//Array access methods
+const leaderboard = ['Harry', 'Lua', 'Hermione', 'Bellatrix']; //DON'T TOUCH THIS LINE!
+// YOUR CODE BELOW HERE:
+leaderboard[1]='Luna'
+leaderboard[3]='Draco'
+//Array methods
+const planets = ['The Moon','Venus', 'Earth', 'Mars', 'Jupiter']; //DO NOT TOUCH THIS LINE!
+// YOUR CODE GOES BELOW THIS LINE:
+planets.shift('The Moon')
+planets.push('Saturn')
+planets.unshift('Mercury')
+//Nested Arrays
+// DO NOT TOUCH!!! (please)
+const airplaneSeats = [
+    ['Ruth', 'Anthony', 'Stevie'],
+    ['Amelia', 'Pedro', 'Maya'],
+    ['Xavier', 'Ananya', 'Luis'],
+    ['Luke', null, 'Deniz'],
+    ['Rin', 'Sakura', 'Francisco']
+];
+// YOUR CODE GOES BELOW THIS LINE:
+airplaneSeats[3][1]='Hugo'
