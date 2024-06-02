a2e18ff1f1b18cfb1b6788fd850cef4f447e1867
Arshiya Kishore <135497076+arshiyaakishore@users.noreply.github.com>
2024-06-02 12:19:11 +0530

    Update javascript.js

--- javascript.js
+++ javascript.js
@@ -19,4 +19,4 @@ const mystery = 'P56p097'; //CHANGE THIS VALUE TO MAKE THE CONDITIONAL BELOW TRU
 if(mystery[0] === 'P' && mystery.length > 5 && mystery.indexOf('7') !== -1){
     console.log("YOU GOT IT!!!");
 }
----
+-----------
