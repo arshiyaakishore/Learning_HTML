e841deeb6a0cce59d5b2c68a286aac231155c7eb
Arshiya Kishore <135497076+arshiyaakishore@users.noreply.github.com>
2024-06-06 18:09:45 +0530

    Update javascript.js

--- javascript.js
+++ javascript.js
@@ -59,3 +59,59 @@ for(let i=0; i<people.length; i++)
     console.log(people[i].toUpperCase())
 }
 
+//Functions
+function printHeart()
+{
+    console.log("<3");
+}
+printHeart()
+
+function isSnakeEyes(die1, die2) {
+  if (die1 === 1 && die2 === 1) {
+    console.log('Snake Eyes!');
+  } else {
+    console.log('Not Snake Eyes!');
+  }
+}
+
+function multiply(num1, num2) {
+  return num1 * num2;
+}
+
+function returnDay(num) {
+  const days = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday'];
+  if (num < 1 || num > 7) {
+    return null;
+  } else {
+    return days[num - 1];
+  }
+}
+
+// Another approach:
+function returnDay(num) {
+  const days = [null, 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday'];
+  if (num < 1 || num > 7) {
+    return null;
+  } else {
+    return days[num];
+  }
+}
+
+// Some more different approaches:
+
+function returnDay(num) {
+  if (num < 1 || num > 7) {
+    return null;
+  }
+  const days = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday'];
+  return days[num - 1];
+}
+
+function returnDay(num) {
+  if (num >= 1 && num <= 7) {
+    const days = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday'];
+    return days[num - 1];
+  }
+  return null;
+}
+
